828cbe6f40e327d290ef5e1b0e86850875dfb8cd
Prajjwal <howtoshout@gmail.com>
2022-05-13 01:14:01 +0530

    commit

---
+++ pages/compononets/heading.js
@@ -0,0 +1,8 @@
+export default function Heading (){
+
+    return (
+        <div className="heading" contentEditable="true">
+            <h1>Heading</h1>
+        </div>
+    )
+}
---
+++ pages/compononets/textarea.js
@@ -0,0 +1,7 @@
+export default function TextArea () {
+    return (
+        <div className="content" contentEditable="true">
+            Just Write Anything
+            </div>
+    )
+}
