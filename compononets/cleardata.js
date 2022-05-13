d415c369fe0f85eadda76a4a53f47525bcf06683
Prajjwal <howtoshout@gmail.com>
2022-05-14 01:10:26 +0530

    commit

--- compononets/cleardata.js
+++ compononets/cleardata.js
@@ -1,6 +1,12 @@
-export default function  () {
+export default function ClearData () {
+
+    function clearData() {
+        localStorage.clear();
+        window.location.reload(true);
+      }
+    
 
     return (
-        <button className="button" type="button" onClick={() => localStorage.clear() }>Clear Data</button>
+        <button className="button" type="button" onClick={clearData}>Clear Data</button>
     )
 }
