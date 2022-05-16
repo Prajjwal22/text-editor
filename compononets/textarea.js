8985fa4ac5831243a8fe1270260e32e2e2c2191a
Prajjwal <howtoshout@gmail.com>
2022-05-16 23:54:56 +0530

    commit

--- compononets/textarea.js
+++ compononets/textarea.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ClearData from "./clearData";
 
 
 export default function TextArea (props) {
@@ -32,10 +33,25 @@ return (
     className="container" value={content}>
     </textarea> 
 
-    <div>
+    <div className="details">
         <p> You Text Summary: {content.trim().split(" ").length} words &amp; {content.length} characters</p>
-    </div>
+<button className="button"
+                            onClick={() => {
+                                // Credit to https://www.codegrepper.com/code-examples/javascript/javascript+download+text+as+txt+file for this snippet
+                                const element = document.createElement("a")
+                                element.setAttribute(
+                                    "href",
+                                    "data:text/plain;charset=utf-8," +
+                                        encodeURIComponent(content)
+                                )
+                                element.setAttribute("download", "mynotes")
+                                element.click()
+                            }}>
+                            Download Text File
+                        </button>
+                        <ClearData/>          
 
+    </div>
     </div>
 
 )
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
         <Navbar/>
         <Heading/>
         <TextArea/>
-        <ClearData/>
       </div>
       
   )
